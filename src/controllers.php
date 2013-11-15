3ebf97951afca7c3d980796aed2211cd93dcb8f9
seb <seb@big>
2013-11-16 00:53:53 +0100

    affichage du formulaire (bidon)

--- src/controllers.php
+++ src/controllers.php
@@ -5,14 +5,53 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Silex\Provider\FormServiceProvider;
+//use Symfony\Component\Locale;
+use Silex\Provider\TranslationServiceProvider;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
+
+
+$app->register(new FormServiceProvider());
+$app->register(new TranslationServiceProvider());
 
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html', array());
 })
 ->bind('homepage')
 ;
+
+$app->match('/form', function(Request $request) use($app) {
+// some default data for when the form is displayed the first time
+    $data = array(
+        'name' => 'Your name',
+        'email' => 'Your email',
+    );
+
+    $form = $app['form.factory']->createBuilder('form', $data)
+        ->add('name')
+        ->add('email')
+        ->add('gender', 'choice', array(
+            'choices' => array(1 => 'male', 2 => 'female'),
+            'expanded' => true,
+        ))
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+
+        // do something with the data
+
+        // redirect somewhere
+	return 'Redirect to implement';
+        // return $app->redirect('...');
+    }
+
+    // display the form
+    return $app['twig']->render('form.html', array('form' => $form->createView()));
+});
 
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
