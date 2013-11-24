799a78647c6a623bffcd0de4353de3bde9057dc0
seb <seb@localhost.localdomain>
2013-11-24 21:40:59 +0100

    correct path in web/index.example.php

--- web/index.example.php
+++ web/index.example.php
@@ -8,9 +8,9 @@ $_app_dir = __DIR__.'/..';
 
 ini_set('display_errors', 0);
 
-require_once $_app_dir.'/../vendor/autoload.php';
+require_once $_app_dir.'/vendor/autoload.php';
 
-$app = require $_app_dir.'/../src/app.php';
-require $_app_dir.'/../config/prod.php';
-require $_app_dir.'/../src/controllers.php';
+$app = require $_app_dir.'/src/app.php';
+require $_app_dir.'/config/prod.php';
+require $_app_dir.'/src/controllers.php';
 $app->run();
