5d159eec025f606743fd7a5b895cd5ec91be7891
SebSept <sebastienmonterisi@yahoo.fr>
2014-01-17 22:55:01 +0100

    added nodejs + gruntjs config files
    + ignored node_modules/

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+// Project configuration
+module.exports = function(grunt) {
+
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // },
+    cssmin: {
+      minify: {
+        report: 'min',
+        expand: true,
+        cwd: 'src/css/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'web/css/',
+        ext: '.css'
+      }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin']);
+
+};
